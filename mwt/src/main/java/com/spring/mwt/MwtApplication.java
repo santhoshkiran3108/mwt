1238e5fed5b3612b9d8f079b5551e9336136ca63
santhoshkiran3108 <santhoshkiran3108@gmail.com>
2020-11-03 00:22:19 +0530

    test

---
+++ mwt/src/main/java/com/spring/mwt/MwtApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwtApplication.class, args);
+	}
+
+}
